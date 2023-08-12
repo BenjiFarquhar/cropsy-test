6d2d78ed7cc09f6f089e69c71533c6672c406d85
Benji <farquhar.benji@gmail.com>
2023-08-12 16:59:31 +1200

    Add app bar and grid to hold filters and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import CpAppBar from "./presentation/components/layout/AppBar";
+import { Box, Container, CssBaseline } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import { blue, red } from "@mui/material/colors";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <CssBaseline />
+      <CpAppBar></CpAppBar>
+      <Grid container spacing={3}>
+        <Grid sx={{ width: "400px", height: "600px", backgroundColor: red }}>
+          <Item>left</Item>
+        </Grid>
+        <Grid xs>
+          <Item>right</Item>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
---
+++ src/presentation/theme.ts
@@ -0,0 +1,40 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#f2f2f7",
+    },
+    background: {
+      default: "#ffffff",
+      paper: "#ffffff",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "#000000",
+    },
+  },
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "text" },
+          style: {
+            backgroundColor: "transparent",
+            color: "#000000",
+          },
+        },
+        {
+          props: { variant: "contained" },
+          style: {
+            backgroundColor: "#5cc974",
+            color: "#FFFFFF",
+          },
+        },
+      ],
+    },
+  },
+});
+
+export default theme;
